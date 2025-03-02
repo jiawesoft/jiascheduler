7dac892aa9ac69b7112a7b609a73088533f0d5e9
iwannay <772648576@qq.com>
2025-03-02 12:45:31 +0800

    Update version 1.0.4

--- openapi/src/lib.rs
+++ openapi/src/lib.rs
@@ -139,7 +139,7 @@ m = g(r.sub, p.sub) && g2(r.obj, p.obj) && r.act == p.act
 "#;
 
 const GIT_VERSION: &str = git_version!();
-const APP_VERSION: &str = "1.0.3";
+const APP_VERSION: &str = "1.0.4";
 
 fn get_version() -> String {
     format!("{APP_VERSION}-{GIT_VERSION}")
